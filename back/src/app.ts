d8ee255d6165ac8de6e3cd6bde4e3f269f568c15
Marco Fornaciari <mfornaciari@gmail.com>
2023-11-04 16:22:29 -0300

    use verifyClient to check userId

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,95 +1,102 @@
-import type { WebSocket } from 'ws'
-import type { FastifyInstance, FastifyServerOptions } from 'fastify'
-import type { Card } from './cards'
-import fastify from 'fastify'
-import fastifyWebsocket from '@fastify/websocket'
-import { buildPlayer } from './buildPlayer'
-import { cards } from './cards'
-import { serializeMatch } from './serializeMatch'
+import type { WebSocket } from "ws";
+import type { FastifyInstance, FastifyServerOptions } from "fastify";
+import type { Card } from "./cards";
+import fastify from "fastify";
+import fastifyWebsocket from "@fastify/websocket";
+import { buildPlayer } from "./buildPlayer";
+import { cards } from "./cards";
+import { serializeMatch } from "./serializeMatch";
 
 export type Match = {
-  players: Player[]
-  openCard: Card
-}
+  players: Player[];
+  openCard: Card;
+};
 export type Player = {
-  id: string
-  matchId: number
-  cards: Card[]
-  socket: WebSocket
-  currentPlayer: boolean
-  isAlive: boolean
-}
+  id: string;
+  matchId: number;
+  cards: Card[];
+  socket: WebSocket;
+  currentPlayer: boolean;
+  isAlive: boolean;
+};
 
-const shuffledCards = cards.sort(() => Math.random() - 0.5)
-const openCard = shuffledCards.splice(0, 1)[0]
+const shuffledCards = cards.sort(() => Math.random() - 0.5);
+const openCard = shuffledCards.splice(0, 1)[0];
 const match: Match = {
   players: [],
   openCard,
-}
-let matchId = 1
+};
+let matchId = 1;
 
 export async function build(options: FastifyServerOptions = {}): Promise<FastifyInstance> {
-  const app = fastify(options)
-  await app.register(fastifyWebsocket)
+  const app = fastify(options);
+  await app.register(fastifyWebsocket, {
+    options: {
+      verifyClient: (info, next) => {
+        if (info.req.headers["sec-websocket-protocol"] == null) {
+          next(false);
+          return;
+        }
+        next(true);
+      },
+    },
+  });
 
-  app.get('/', { websocket: true }, (connection, request) => {
-    const socket = connection.socket
-    const userId = request.headers['sec-websocket-protocol']
+  app.get("/", { websocket: true }, (connection, request) => {
+    const socket = connection.socket;
+    const userId = request.headers["sec-websocket-protocol"];
 
-    if (userId == null) {
-      socket.terminate()
-      return
-    }
+    if (userId == null) return;
 
     for (const player of match.players) {
-      if (player.id === userId) player.socket.terminate()
+      if (player.id === userId) player.socket.terminate();
     }
 
-    const existingPlayer = match.players.find(player => player.id === userId)
+    const existingPlayer = match.players.find(player => player.id === userId);
     if (existingPlayer == null) {
-      addPlayer(socket, userId)
+      addPlayer(socket, userId);
     } else {
-      existingPlayer.socket = socket
+      existingPlayer.socket = socket;
     }
 
-    for (const player of match.players) sendMatch(player)
-  })
+    for (const player of match.players) sendMatch(player);
+  });
 
-  return await Promise.resolve(app)
+  return await Promise.resolve(app);
 }
 
 function addPlayer(socket: WebSocket, userId: string): void {
-  const currentPlayer = match.players.length === 0
+  const currentPlayer = match.players.length === 0;
   const newPlayer = buildPlayer({
     cards: shuffledCards,
     matchId,
     currentPlayer,
     socket,
     userId,
-  })
-  matchId += 1
-  match.players.push(newPlayer)
+  });
+  matchId += 1;
+  match.players.push(newPlayer);
 
-  startHeartbeat(newPlayer)
-  socket.on('pong', () => {
-    newPlayer.isAlive = true
-  })
+  startHeartbeat(newPlayer);
+  socket.on("pong", () => {
+    newPlayer.isAlive = true;
+  });
 }
 
 function startHeartbeat(player: Player): void {
-  const socket = player.socket
+  const socket = player.socket;
   setInterval(() => {
     if (!player.isAlive) {
-      match.players = match.players.filter(matchPlayer => matchPlayer.id !== player.id)
-      socket.terminate()
-      return
+      match.players = match.players.filter(matchPlayer => matchPlayer.id !== player.id);
+      socket.terminate();
+      return;
     }
-    player.isAlive = false
-    socket.ping()
-  }, 30000)
+    player.isAlive = false;
+    socket.ping();
+  }, 30000);
 }
 
 function sendMatch(player: Player): void {
-  const payload = serializeMatch(player, match)
-  player.socket.send(JSON.stringify(payload))
+  const payload = serializeMatch(player, match);
+  player.socket.send(JSON.stringify(payload));
 }
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,62 +1,62 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 type Card = {
-  number: number
-}
+  number: number;
+};
 
 type CurrentPlayer = {
-  cards: Card[]
-  currentPlayer: boolean
-  matchId: number
-}
+  cards: Card[];
+  currentPlayer: boolean;
+  matchId: number;
+};
 type OtherPlayer = {
-  currentPlayer: boolean
-  numberOfCards: number
-  matchId: number
-}
+  currentPlayer: boolean;
+  numberOfCards: number;
+  matchId: number;
+};
 type Match = {
-  player: CurrentPlayer | null
-  openCard: Card | null
-  otherPlayers: OtherPlayer[]
-}
+  player: CurrentPlayer | null;
+  openCard: Card | null;
+  otherPlayers: OtherPlayer[];
+};
 
 export function App() {
-  const [_client, setClient] = useState<WebSocket | null>(null)
+  const [_client, setClient] = useState<WebSocket | null>(null);
   const [match, setMatch] = useState<Match>({
     player: null,
     openCard: null,
     otherPlayers: [],
-  })
+  });
 
-  const { player, openCard, otherPlayers } = match
+  const { player, openCard, otherPlayers } = match;
 
   useEffect(() => {
-    let userId = sessionStorage.getItem('userId')
+    let userId = sessionStorage.getItem("userId");
     if (userId == null) {
-      const newId = crypto.randomUUID()
-      sessionStorage.setItem('userId', newId)
-      userId = newId
+      const newId = crypto.randomUUID();
+      sessionStorage.setItem("userId", newId);
+      userId = newId;
     }
-    const newClient = new WebSocket('ws://localhost:3001', userId)
-    newClient.addEventListener('message', event => {
-      console.log(event.data)
-      setMatch(JSON.parse(event.data))
-    })
-    setClient(newClient)
+    const newClient = new WebSocket("ws://localhost:3001", userId);
+    newClient.addEventListener("message", event => {
+      console.log(event.data);
+      setMatch(JSON.parse(event.data));
+    });
+    setClient(newClient);
     return () => {
-      newClient.close()
-    }
-  }, [])
+      newClient.close();
+    };
+  }, []);
 
-  const started = player != null && openCard != null && match.otherPlayers.length >= 1
+  const started = player != null && openCard != null && match.otherPlayers.length >= 1;
 
   if (!started)
     return (
       <main>
         <p>Aguardando início da partida...</p>
       </main>
-    )
+    );
 
   const otherPlayerCards = otherPlayers.map(otherPlayer => {
     return (
@@ -67,8 +67,8 @@ export function App() {
             <li key={`${otherPlayer.matchId}-${index}`} className="card" />
           ))}
       </ol>
-    )
-  })
+    );
+  });
   const playerCards = (
     <ol>
       {player.cards.map((card, index) => (
@@ -77,7 +77,7 @@ export function App() {
         </li>
       ))}
     </ol>
-  )
+  );
 
   return (
     <main>
@@ -85,5 +85,5 @@ export function App() {
       {openCard && <div className="card">{openCard.number}</div>}
       {playerCards}
     </main>
-  )
+  );
 }
