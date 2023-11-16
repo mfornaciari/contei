46ec9098c5a91a15ae7d60c6a4a4a2cd37576e3d
Marco Fornaciari <marco.fornaciari@rebase.com.br>
2023-11-16 18:18:23 -0300

    test first player connection

--- back/src/main.test.ts
+++ back/src/main.test.ts
@@ -1,18 +1,27 @@
-import { describe, expect, it } from "bun:test";
+import { afterAll, beforeAll, describe, expect, it } from "bun:test";
 import { app } from "./main";
+import { type Server } from "bun";
 
 describe("app", () => {
+  let server: Server | null;
+
+  beforeAll(() => {
+    server = app.server;
+  });
+
+  afterAll(async () => {
+    await app.stop();
+  });
+
   it("does not allow connection if no player ID is received", async () => {
-    const server = app.server;
     if (server != null) {
       const client = new WebSocket(`ws://${server.hostname}:${server.port}`);
       let resolveHandler: (value: string) => void;
       const message = new Promise<string>(resolve => {
         resolveHandler = resolve;
       });
-      client.addEventListener("message", async event => {
+      client.addEventListener("message", event => {
         resolveHandler(event.data.toString());
-        await app.stop();
       });
 
       expect(await message).toEqual("No player ID received. Closing connection");
@@ -20,22 +29,28 @@ describe("app", () => {
     }
   });
 
-  it.todo("adds new player if player ID is received", async () => {
-    const server = app.server;
+  it("adds new player if player ID is received", async () => {
     const playerId = crypto.randomUUID();
     if (server != null) {
-      const client = new WebSocket(`ws://${server.hostname}:${server.port}`, playerId);
+      const client = new WebSocket(`ws://${server.hostname}:${server.port}`, {
+        headers: { "sec-websocket-protocol": playerId },
+      });
       let resolveHandler: (value: string) => void;
       const message = new Promise<string>(resolve => {
         resolveHandler = resolve;
       });
-      client.addEventListener("message", async event => {
+      client.addEventListener("message", event => {
         resolveHandler(event.data.toString());
         client.close();
-        await app.stop();
       });
 
-      expect(await message).toEqual("No player ID received. Closing connection");
+      const result = JSON.parse(await message);
+
+      expect(result.player.cards).toBeArrayOfSize(7);
+      expect(result.player.currentPlayer).toBeTrue();
+      expect(result.player.number).toEqual(1);
+      expect(result.otherPlayers).toBeArrayOfSize(0);
+      expect(result.openCard).toBeDefined();
     }
   });
 });
