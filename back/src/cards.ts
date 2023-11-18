89cf78ef7b08651b9b5c39dbaa998b5842a8eb7b
Marco Fornaciari <mfornaciari@gmail.com>
2023-11-18 09:47:23 -0300

    add color to cards

--- back/src/cards.ts
+++ back/src/cards.ts
@@ -1,17 +1,50 @@
 export type Card = {
+  color: CardColor;
   number: number;
 };
 
-export function buildCards(): Card[] {
-  const cards1: Card[] = Array(9).fill({ number: 1 });
-  const cards2: Card[] = Array(9).fill({ number: 2 });
-  const cards3: Card[] = Array(9).fill({ number: 3 });
-  const cards4: Card[] = Array(6).fill({ number: 4 });
-  const cards5: Card[] = Array(6).fill({ number: 5 });
-  const cards6: Card[] = Array(6).fill({ number: 6 });
-  const cards7: Card[] = Array(3).fill({ number: 7 });
-  const cards8: Card[] = Array(3).fill({ number: 8 });
-  const cards9: Card[] = Array(3).fill({ number: 9 });
+export type CardColor = "blue" | "pink" | "purple";
 
-  return cards1.concat(cards2, cards3, cards4, cards5, cards6, cards7, cards8, cards9);
+type BuildCardData = {
+  color: CardColor;
+  number: number;
+  amount: number;
+};
+
+const buildCardsData: BuildCardData[] = [
+  { color: "blue", number: 1, amount: 3 },
+  { color: "blue", number: 2, amount: 3 },
+  { color: "blue", number: 3, amount: 3 },
+  { color: "blue", number: 4, amount: 2 },
+  { color: "blue", number: 5, amount: 2 },
+  { color: "blue", number: 6, amount: 2 },
+  { color: "blue", number: 7, amount: 1 },
+  { color: "blue", number: 8, amount: 1 },
+  { color: "blue", number: 9, amount: 1 },
+  { color: "pink", number: 1, amount: 3 },
+  { color: "pink", number: 2, amount: 3 },
+  { color: "pink", number: 3, amount: 3 },
+  { color: "pink", number: 4, amount: 2 },
+  { color: "pink", number: 5, amount: 2 },
+  { color: "pink", number: 6, amount: 2 },
+  { color: "pink", number: 7, amount: 1 },
+  { color: "pink", number: 8, amount: 1 },
+  { color: "pink", number: 9, amount: 1 },
+  { color: "purple", number: 1, amount: 3 },
+  { color: "purple", number: 2, amount: 3 },
+  { color: "purple", number: 3, amount: 3 },
+  { color: "purple", number: 4, amount: 2 },
+  { color: "purple", number: 5, amount: 2 },
+  { color: "purple", number: 6, amount: 2 },
+  { color: "purple", number: 7, amount: 1 },
+  { color: "purple", number: 8, amount: 1 },
+  { color: "purple", number: 9, amount: 1 },
+];
+
+export function buildCards(): Card[] {
+  return buildCardsData
+    .map(data => {
+      return Array(data.amount).fill({ color: data.color, number: data.number });
+    })
+    .flat();
 }
--- back/tests/cards.test.ts
+++ back/tests/cards.test.ts
@@ -1,11 +1,41 @@
 import { describe, expect, it } from "bun:test";
-import { buildCards } from "../src/cards";
+import { type Card, type CardColor, buildCards } from "../src/cards";
 
 describe("buildCards", () => {
   it("returns array of 54 cards", () => {
     const cards = buildCards();
 
     expect(cards).toBeArrayOfSize(54);
-    expect(cards.map(card => card.number)).toBeArrayOfSize(54);
+    expect(cards.filter(card => filterCard(card, 1, "blue"))).toBeArrayOfSize(3);
+    expect(cards.filter(card => filterCard(card, 2, "blue"))).toBeArrayOfSize(3);
+    expect(cards.filter(card => filterCard(card, 3, "blue"))).toBeArrayOfSize(3);
+    expect(cards.filter(card => filterCard(card, 4, "blue"))).toBeArrayOfSize(2);
+    expect(cards.filter(card => filterCard(card, 5, "blue"))).toBeArrayOfSize(2);
+    expect(cards.filter(card => filterCard(card, 6, "blue"))).toBeArrayOfSize(2);
+    expect(cards.filter(card => filterCard(card, 7, "blue"))).toBeArrayOfSize(1);
+    expect(cards.filter(card => filterCard(card, 8, "blue"))).toBeArrayOfSize(1);
+    expect(cards.filter(card => filterCard(card, 9, "blue"))).toBeArrayOfSize(1);
+    expect(cards.filter(card => filterCard(card, 1, "pink"))).toBeArrayOfSize(3);
+    expect(cards.filter(card => filterCard(card, 2, "pink"))).toBeArrayOfSize(3);
+    expect(cards.filter(card => filterCard(card, 3, "pink"))).toBeArrayOfSize(3);
+    expect(cards.filter(card => filterCard(card, 4, "pink"))).toBeArrayOfSize(2);
+    expect(cards.filter(card => filterCard(card, 5, "pink"))).toBeArrayOfSize(2);
+    expect(cards.filter(card => filterCard(card, 6, "pink"))).toBeArrayOfSize(2);
+    expect(cards.filter(card => filterCard(card, 7, "pink"))).toBeArrayOfSize(1);
+    expect(cards.filter(card => filterCard(card, 8, "pink"))).toBeArrayOfSize(1);
+    expect(cards.filter(card => filterCard(card, 9, "pink"))).toBeArrayOfSize(1);
+    expect(cards.filter(card => filterCard(card, 1, "purple"))).toBeArrayOfSize(3);
+    expect(cards.filter(card => filterCard(card, 2, "purple"))).toBeArrayOfSize(3);
+    expect(cards.filter(card => filterCard(card, 3, "purple"))).toBeArrayOfSize(3);
+    expect(cards.filter(card => filterCard(card, 4, "purple"))).toBeArrayOfSize(2);
+    expect(cards.filter(card => filterCard(card, 5, "purple"))).toBeArrayOfSize(2);
+    expect(cards.filter(card => filterCard(card, 6, "purple"))).toBeArrayOfSize(2);
+    expect(cards.filter(card => filterCard(card, 7, "purple"))).toBeArrayOfSize(1);
+    expect(cards.filter(card => filterCard(card, 8, "purple"))).toBeArrayOfSize(1);
+    expect(cards.filter(card => filterCard(card, 9, "purple"))).toBeArrayOfSize(1);
   });
 });
+
+function filterCard(card: Card, number: number, color: CardColor): boolean {
+  return card.number === number && card.color === color;
+}
