a96680486250b9679e7a266fafe4b7e6e18a79c1
Marco Fornaciari <mfornaciari@gmail.com>
2024-03-20 09:57:51 -0300

    WIP: handle passing on backend

--- back/src/plugins/contei.ts
+++ back/src/plugins/contei.ts
@@ -1,9 +1,13 @@
 import { type Setup } from "./setup";
-import { serializeMatchForPlayer } from "../match";
+import { t } from "elysia";
+import { type Match, serializeMatchForPlayer } from "../match";
 import { addPlayer } from "../player";
 
 export function contei(app: Setup): Setup {
   return app.ws("/game", {
+    body: t.Object({
+      type: t.Literal("pass"),
+    }),
     open(ws) {
       const ipAddress = ws.remoteAddress;
       const match = app.store.match;
@@ -22,5 +26,28 @@ export function contei(app: Setup): Setup {
         app.server?.publish(player.id, JSON.stringify(serializedMatch));
       }
     },
+    message(ws, { type }) {
+      const match = app.store.match;
+      const ipAddress = ws.remoteAddress;
+      if (type === "pass") {
+        handlePass(match, ipAddress);
+        for (const player of match.players) {
+          const serializedMatch = serializeMatchForPlayer(player.id, match);
+          app.server?.publish(player.id, JSON.stringify(serializedMatch));
+        }
+      }
+    },
   });
+}
+
+function handlePass(match: Match, ipAddress: string): void {
+  const player = match.players.find(player => player.ipAddress === ipAddress);
+  if (player != null) {
+    const card = match.cards.pop();
+    if (card != null) player.cards.push(card);
+    player.currentPlayer = false;
+    let nextPlayer = match.players.find(otherPlayer => otherPlayer.number === player.number + 1);
+    if (nextPlayer == null) nextPlayer = match.players.find(otherPlayer => otherPlayer.number === 1);
+    if (nextPlayer != null) nextPlayer.currentPlayer = true;
+  }
 }
